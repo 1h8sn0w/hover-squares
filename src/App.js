dee8b054ab118302cada4b3798ec27f3026c5f98
Volodymyr <citynight.immortal@gmail.com>
2021-05-29 22:06:30 +0300

    material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button, Container, FormControl, InputLabel, makeStyles, MenuItem, Select} from "@material-ui/core";
+import {useState} from "react";
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        display: 'block'
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    const [mode, setMode] = useState({"easyMode": {"field": 5}});
+    return (
+        <Container maxWidth="sm">
+            <FormControl className={classes.formControl}>
+                <InputLabel>Pick mode</InputLabel>
+                <Select
+                    value={12}
+                    onChange={() => {
+                    }}
+                >
+                    <MenuItem value={mode.easyMode.field}>Easy mode</MenuItem>
+                    <MenuItem value={10}>Normal mode</MenuItem>
+                    <MenuItem value={20}>Hard mode</MenuItem>
+                </Select>
+                <Button>START</Button>
+            </FormControl>
+        </Container>
+    );
 }
 
 export default App;
+
+
+// {
+//     "easyMode": {
+//     "field": 5
+// },
+//     "normalMode": {
+//     "field": 10
+// },
+//     "hardMode": {
+//     "field": 15
+// }
+// }
